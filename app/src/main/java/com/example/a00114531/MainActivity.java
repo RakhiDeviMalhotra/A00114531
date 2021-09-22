d2bd0379105ff38655241e07b03a314089d68d88
Rakhi Malhotra <rakhidevi07@gmail.com>
2021-09-22 13:23:30 -0400

    A00114531- MAD(Mobile Application Development)

--- app/src/main/java/com/example/a00114531/MainActivity.java
+++ app/src/main/java/com/example/a00114531/MainActivity.java
@@ -74,5 +74,6 @@ public class MainActivity extends AppCompatActivity {
                 || super.onSupportNavigateUp();
 
         //This is my Assignment of Android Development//
+        //My Name is Rakhi Devi Malhotra. I am a student of Canadore college Scarborough.//
     }
 }
